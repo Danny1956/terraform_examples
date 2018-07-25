4c354e7ecf6b89d66e3a897bc540020dda4cf804
Danny Shinder <DShinder@utilitywarehouse.co.uk>
2018-07-25 14:25:18 +0100

    initial commit

---
+++ tf_simple_ec2_instance_with_elastic_ip/aws1.tf
@@ -0,0 +1,23 @@
+#simple aws ec2 instance with eip
+
+provider "aws"{
+  region = "eu-west-1"
+}
+resource "aws_instance" "${var.aws_instance_name}" {
+  ami = "ami-7c491f05"
+  instance_type = "t2.micro"
+  key_name = "${var.keyname}"
+
+  tags {
+    Name = "${var.tag_name}",
+    Project = "${var.tag_project}"
+  }
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.${var.aws_instance_name}.id}"
+}
+
+output "aws_instance-ec2_eip"{
+  value = "${aws_eip.ip.public_ip}"
+}
---
+++ tf_simple_ec2_instance_with_elastic_ip/variables.tf
@@ -0,0 +1,12 @@
+variable "keyname"{
+  default = ""
+}
+variable "tag_project"{
+  default = "Demo1"
+}
+variable "tag_name"{
+  default = "Plu 2 Server"
+}
+variable "aws_instance_name"{
+  default = ""
+}
